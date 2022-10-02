410b03a8eab8a37cd0654001b9f641d12ceb859e
idsefa <102581868+idsefa@users.noreply.github.com>
2022-10-02 12:28:21 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -2,3 +2,4 @@ git clone https://github.com/CHN-beta/rkp-ipid package/rkp-ipid
 
 svn checkout https://github.com/openwrt/packages/trunk/libs/libnetfilter-queue package/libs/libnetfilter-queue
 git clone https://github.com/Zxilly/UA2F package/UA2F
+git clone https://github.com/GJXS1980/ODP package/ODP
